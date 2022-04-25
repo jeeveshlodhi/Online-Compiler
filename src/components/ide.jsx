60bebcaf467915927519c31aa8b59b4c0118b377
jeeveshlodhi <lodhijeevesh2000@gmail.com>
2022-04-26 00:45:56 +0530

    monaco added

--- src/components/ide.jsx
+++ src/components/ide.jsx
@@ -1,17 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Prism from "prismjs";
 import '../style/ide.css'
+import Editor from "@monaco-editor/react";
+import { monaco } from "@monaco-editor/react";
 
 const Ide = () => {
     const[line, setLine] = useState(1)
     const[textData, setTextData] = useState('')
 
+    const handleEditorDidMount = () => {
+
+    }
+
+    // useEffect(()=>{
+    //     monaco.editor.defineTheme('my-theme', {
+    //         base: 'vs',
+    //         inherit: true,
+    //         rules: [],
+    //         colors: {
+    //           'editor.background': '#000000',
+    //         },
+    //   });
+    // })
     return(
         <div className='ideWrapper'>
-            <pre className='ide'>
-                <code className=''>
-                </code>
-            </pre>
+            <Editor
+                height="90vh"
+                width="60vw"
+                defaultLanguage="python"
+                defaultValue="// some comment"
+                theme="vs-dark"
+                line="2"
+                options={{
+                    minimap: {
+                    enabled: false,
+                    },
+                    fontSize: 18,
+                    cursorStyle: "block",
+                    wordWrap: "on",
+                }}
+                editorDidMount={handleEditorDidMount}
+            />
         </div>
     )
 }
